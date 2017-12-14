ef818b3c11f68d0f04fe26dd15470bb099738e91
yaroslav-orel <yaroslav.orel.kh@gmail.com>
2017-12-14 12:48:59 +0200

    Fix compilation issues

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,4 @@
 import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
 import com.datastax.driver.mapping.Mapper;
 import com.datastax.driver.mapping.MappingManager;
 import lombok.val;
@@ -7,7 +6,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.util.Collections;
 import java.util.Properties;
 import java.util.function.Function;
 
@@ -15,7 +13,6 @@ import static com.google.common.base.Splitter.on;
 import static com.google.common.collect.Streams.stream;
 import static java.lang.Float.parseFloat;
 import static java.lang.Integer.parseInt;
-import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 
 public class Application {
@@ -29,7 +26,6 @@ public class Application {
 
             while (true){
                 val records = kafkaConsumer.poll(1000);
-
                 stream(records)
                         .map(toUservisit())
                         .forEach(mapper::save);
@@ -42,9 +38,10 @@ public class Application {
     private static KafkaConsumer<String, String> initConsumer() {
         val props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "main");
         props.put("key.deserializer", StringDeserializer.class.getName());
         props.put("value.deserializer", StringDeserializer.class.getName());
-        val conusmer = new KafkaConsumer<>(props);
+        val conusmer = new KafkaConsumer<String, String>(props);
 
         conusmer.subscribe(singletonList("uservisits"));
         return conusmer;
